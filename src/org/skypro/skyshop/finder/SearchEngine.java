4921221e9da2c7c01463f78cbd60cb4bf20eb796
StanislavKlyuev <patriek@rambler.ru>
2025-12-06 23:05:08 +0300

    JC.OOP.HW_task_5

--- src/org/skypro/skyshop/finder/SearchEngine.java
+++ src/org/skypro/skyshop/finder/SearchEngine.java
@@ -2,47 +2,30 @@ package org.skypro.skyshop.finder;
 
 import org.skypro.skyshop.exceptions.BestResultNotFound;
 import org.skypro.skyshop.interfaces.Searchable;
-import org.skypro.skyshop.product.Product;
 
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchEngine {
 
-    private Searchable[] searchables;
+    private List<Searchable> searchables;
 
-    public SearchEngine(int size) {
-        if (size <= 0) {
-            throw new IllegalArgumentException("Введено число меньше \"1\".");
-        }
-        this.searchables = new Searchable[size];
+    public SearchEngine() {
+        this.searchables = new ArrayList<>();
     }
 
     public void add(Searchable searchable) {
-        boolean isAdd = false;
-        while (!isAdd) {
-            for (int i = 0; i < searchables.length; i++) {
-                if (searchables[i] == null) {
-                    searchables[i] = searchable;
-                    isAdd = true;
-                    break;
-                }
-            }
-        }
+        searchables.add(searchable);
     }
 
-    public Searchable[] search(String string) {
-        Searchable[] find = new Searchable[5];
-        int count = 0;
+    public List<Searchable> search(String string) {
+        List<Searchable> find = new ArrayList<>();
         string = string.toLowerCase();
         for (Searchable searchable : searchables) {
             String str1 = searchable.getStringRepresentation().toLowerCase();
             String str2 = searchable.getContentType().toLowerCase();
             if (str1.contains(string) || str2.contains(string)) {
-                find[count] = searchable;
-                count++;
-            }
-            if (count == 5) {
-                break;
+                find.add(searchable);
             }
         }
         return find;
